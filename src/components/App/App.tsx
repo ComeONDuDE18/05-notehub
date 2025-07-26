13bc39aef2a0a4cdb1e55905a4781d0ee9d7a1c5
ComeONDuDE18 <vibilous6@gmail.com>
2025-07-26 15:40:24 +0200

    neversleep

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,76 @@
-import React from "react";
-import ReactPaginate from "react-paginate";
+import { useState, useEffect } from "react";
+import { useDebounce } from "use-debounce";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../services/noteService";
+
+
+import NoteList from "../NoteList/NoteList";
+import NoteForm from "../NoteForm/NoteForm";
+import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
-import css from "./App.module.css";
+import Modal from "../Modal/Modal";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import { useQuery } from '@tanstack/react-query';
+
+
+
+import css from "./App.module.css";
+
+export default function App() {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [debouncedSearch] = useDebounce(search, 300);
+
+  useEffect(() => {
+    setPage(1);
+  }, [debouncedSearch]);
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ["notes", page, debouncedSearch],
+    queryFn: () =>
+      fetchNotes(page, 12, debouncedSearch), 
+    placeholderData: keepPreviousData,
+  });
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        
+        <SearchBox value={search} onChange={setSearch} />
+
+        {isLoading && <Loader />}
+
+        {isError && <ErrorMessage />}
+
+        {data && data.totalPages > 1 && (
+          <Pagination
+            currentPage={page}
+            totalPages={data.totalPages}
+            onPageChange={setPage}
+          />
+        )}
+
+        <button
+          className={css.button}
+          type="button"
+          onClick={() => setModalIsOpen(true)}
+        >
+          Create note +
+        </button>
+      </header>
+
+      {isSuccess && data.notes.length > 0 ? (
+        <NoteList notes={data.notes} />
+      ) : (
+        !isLoading && <p>No notes found</p>
+      )}
+
+      {modalIsOpen && (
+        <Modal onClose={() => setModalIsOpen(false)}>
+          <NoteForm onClose={() => setModalIsOpen(false)} />
+        </Modal>
+      )}
+    </div>
+  );
+}
